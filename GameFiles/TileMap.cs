9bd25595fcf8c7abe2e2e02800e83d2326a4c7f3
Tommy Stemler <stemlerthomas@outlook.com>
2023-07-18 21:30:52 -0500

    Continued Work on TileMap Stuff.

--- GameFiles/TileMap.cs
+++ GameFiles/TileMap.cs
@@ -18,13 +18,21 @@ namespace Basic2DGame.GameFiles
     internal class Tile
     {
         public Vector2 Position { get; set; }
+
+        public int TilesetIndex { get; set; }
+
         public Dictionary<string, string> Properties { get; set; }
 
-        public Tile(Vector2 Position)
+        public Tile()
         {
+            // Sets the default properties of the tile.
             Properties = new()
             {
-                { "", "" }
+                { "Collision", "false" },
+                { "Rotation", "0" },
+                { "Visable", "true" },
+                { "Layer", "0" },
+                { "Color", "White"}
             };
         }
 
@@ -40,7 +48,8 @@ namespace Basic2DGame.GameFiles
 
         public void SetProperty(string name, string value)
         {
-            Properties.TryAdd(name, value);
+            if(!Properties.TryAdd(name, value))
+                Properties[name] = value;
         }
 
         public string GetProperty(string name)
@@ -60,9 +69,12 @@ namespace Basic2DGame.GameFiles
         public string Author { get; set; }
         public string Description { get; set; }
         public string Version { get; set; }
-        public string Size { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
         public string TileSize { get; set; }
         public string Tileset { get; set; }
+
+        public Tile[,] TileMap { get; private set; }
 
         public Map(string xmlFilePath)
         {
@@ -82,9 +94,37 @@ namespace Basic2DGame.GameFiles
                 Author = GetNodeValue(metaDataNode, "Author");
                 Description = GetNodeValue(metaDataNode, "Description");
                 Version = GetNodeValue(metaDataNode, "Version");
-                Size = GetNodeValue(metaDataNode, "Size");
+                Width = int.Parse(GetNodeValue(metaDataNode, "Size/X"));
+                Height = int.Parse(GetNodeValue(metaDataNode, "Size/Y"));
                 TileSize = GetNodeValue(metaDataNode, "TileSize");
                 Tileset = GetNodeValue(metaDataNode, "Tileset");
+
+                XmlNode Grid = xmlDoc.SelectSingleNode("/MapData/Grid");
+
+                TileMap = new Tile[Width, Height];
+
+                for (int y = 0; y < Height; y++)
+                {
+                    XmlNode rowNode = Grid.SelectSingleNode($"Row{y}");
+
+                    for (int x = 0; x < Width; x++)
+                    {
+                        Tile tempTile = new();
+                        XmlNode tileNode = rowNode.SelectSingleNode($"Tile{x}");
+
+                        // Sets the position of the tile.
+                        tempTile.Position = new Vector2(x, y);
+
+                        // Sets the tileset index of the tile.
+                        tempTile.TilesetIndex = int.Parse(GetNodeValue(tileNode, "Key"));
+
+                        // Sets the properties of the tile.
+                        foreach (XmlNode propertyNode in tileNode.ChildNodes)
+                        {
+                            tempTile.SetProperty(propertyNode.Name, propertyNode.InnerText);
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
