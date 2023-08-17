5f6ebc39b8c03da8795f6a0ed881365c2cf93aad
Tommy Stemler <stemlerthomas@outlook.com>
2023-08-16 20:11:21 -0500

    Moved the Variables struct to its own file

---
+++ GameFiles/Variables.cs
@@ -0,0 +1,28 @@
+﻿using Basic2DGame.GameFiles.Managers;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic2DGame.GameFiles
+{
+    public struct Variables
+    {
+        public static KeyboardState PreviousKeyboardState { get; set; }
+
+        public static TextureManager Textures { get; set; }
+
+        public static ContentManager Content { get; set; }
+
+        public static GraphicsDeviceManager Graphics { get; set; }
+
+        public static SpriteBatch SpriteBatch { get; set; }
+
+        public static Vector2 TEMPORARY; // DELEAT THIS EVENTUALY
+    }
+}
