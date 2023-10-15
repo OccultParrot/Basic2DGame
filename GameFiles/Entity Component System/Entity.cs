2fc23f375833b5c15b99dd1034482bf2c4003374
Tommy Stemler <stemlerthomas@outlook.com>
2023-10-14 19:03:45 -0500

    Added entity

---
+++ GameFiles/Entity Component System/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Basic2DGame.GameFiles.Entity_Component_System;
+
+public struct Entity
+{
+    private static uint _id = 0;
+    public uint ID { get; private set; }
+    public Entity()
+    {
+        ID = _id++;
+    }
+}
