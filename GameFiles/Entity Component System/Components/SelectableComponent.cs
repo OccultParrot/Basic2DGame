5da0b00346d315987ac6321dfb44f2d1cb24018c
Tommy Stemler <stemlerthomas@outlook.com>
2023-10-25 16:09:20 -0500

    Finished the Component Manager

---
+++ GameFiles/Entity Component System/Components/SelectableComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic2DGame.GameFiles.Entity_Component_System.Components;
+internal class SelectableComponent : IComponent
+{
+    /// <summary>
+    /// Tells if the entity is selected.
+    /// </summary>
+    public bool IsSelected { get; private set; }
+
+    /// <summary>
+    /// This event is raised when the entity is selected.
+    /// </summary>
+    public event EventHandler OnSelected;
+    public SelectableComponent() { }
+
+    void IComponent.Update()
+    {
+        if (IsSelected)
+            // Raise the event
+            OnSelected?.Invoke(this, EventArgs.Empty);
+    }
+    void IComponent.ChangeValue(string valueName, object value)
+    {
+        switch (valueName)
+        {
+            case "IsSelected":
+                IsSelected = (bool)value;
+                break;
+        }
+    }
+}
