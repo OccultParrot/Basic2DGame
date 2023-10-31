b8ddd754f70b07a84a7651909ef7c8621c8fae3e
Tommy Stemler <stemlerthomas@outlook.com>
2023-10-30 21:16:17 -0500

    Removed vestigial using statements. (Good use of vestigial there ; ] )

--- GameFiles/Variables.cs
+++ GameFiles/Variables.cs
@@ -1,6 +1,4 @@
-﻿using Basic2DGame.GameFiles.Managers;
-using Basic2DGame.GameFiles.Sprites;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
